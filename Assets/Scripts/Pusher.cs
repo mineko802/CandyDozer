8cdc17b26b9ef5110ff3605ea412e5d93b8a1c4d
mineko <ttc2550sg0023@edu.tech.ac.jp>
2025-06-05 12:05:53 +0900

    update

--- Assets/Scripts/Pusher.cs
+++ Assets/Scripts/Pusher.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PushBox : MonoBehaviour
+public class Pusher : MonoBehaviour
 {
-    int num = 0;
-    public GameObject gameObject = default;
+    public GameObject pusher = default;
+    public Vector3 defaultPosition;
+    public float pushPower = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        num = 1 + 1;
-        Debug.Log(num);
+        defaultPosition = pusher.transform.localPosition;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = gameObject.transform.position + new Vector3(0,0,-1);
+        float z = pushPower * Mathf.Sin(Time.time);
+        pusher.transform.localPosition = new Vector3(0, 0, z) + defaultPosition;
     }
 }
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooter : MonoBehaviour
+{
+    public GameObject candy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            CandyPop();
+        };
+    }
+    public void CandyPop()
+    {
+        GameObject candyPrefab = Instantiate(candy, transform);
+        candyPrefab.GetComponent<Rigidbody>().AddForce(transform.forward * 10);
+    }
+}
